e588ff68822e953607cd8f3c8ae7943872a9b87b
Lucas Migge de Barros <miggelucas@Mig-MacBook-Pro.local>
2022-07-04 17:03:40 -0300

    Adiciona projeto do xcode realiza first commit

---
+++ testandoGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testandoGit
+//
+//  Created by Lucas Migge de Barros on 04/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
